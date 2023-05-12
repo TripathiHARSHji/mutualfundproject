e98489af68c74a4235127c149f37613336457f0a
Harsh Tripathi <103751175+TripathiHARSHji@users.noreply.github.com>
2023-05-12 10:49:22 +0530

    Add files via upload

---
+++ SearchMF/src/main/java/com/searchmf/SearchMfApplication.java
@@ -0,0 +1,14 @@
+package com.searchmf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchMfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchMfApplication.class, args);
+		
+		
+	}
+}
