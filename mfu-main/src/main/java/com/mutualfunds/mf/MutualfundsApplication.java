17901512854cece89f3c8ccb073b84cee4c11dcd
Harsh Tripathi <103751175+TripathiHARSHji@users.noreply.github.com>
2023-05-17 19:53:51 +0530

    Add files via upload

---
+++ mfu-main/src/main/java/com/mutualfunds/mf/MutualfundsApplication.java
@@ -0,0 +1,15 @@
+package com.mutualfunds.mf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MutualfundsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutualfundsApplication.class, args);
+	}
+
+}
